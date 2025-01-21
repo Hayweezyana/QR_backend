84aa2a230f8a81f7ce423387b77bf002badab79a
hayweezyana <ayotundeadeboyeje@gmail.com>
2025-01-21 16:34:56 +0100

    get

--- server.js
+++ server.js
@@ -12,17 +12,12 @@ app.use(cors({
 
 }));
 
-app.get("/get-color", (_req, res) => {
+app.get("/get-color", (req, res) => {
   visitorCount++;
   const index = Math.floor((visitorCount - 1) / 5) % COLORS.length;
   const color = COLORS[index];
   res.json({ color, visitorCount });
 });
-
-app.get("/", (_req, res) => {
-  res.send("Welcome to the visitor color backend!");
-});
-
 
 app.listen(port, () => {
   console.log(`Server running at https://qr-server-uxza.onrender.com:${port}`);
