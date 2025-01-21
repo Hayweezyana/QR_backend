bd9609c25ea4e6dea49204a9becd33f552589f48
hayweezyana <ayotundeadeboyeje@gmail.com>
2025-01-21 14:57:40 +0100

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+let visitorCount = 0;
+
+const COLORS = ["RED", "BLUE", "GREEN", "YELLOW", "ORANGE", "PURPLE"];
+
+app.use(cors());
+
+app.get("/get-color", (req, res) => {
+  visitorCount++;
+  const index = Math.floor((visitorCount - 1) / 5) % COLORS.length;
+  const color = COLORS[index];
+  res.json({ color, visitorCount });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
